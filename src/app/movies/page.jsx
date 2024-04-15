c2fa462e9313ef0433e18939ab79a666acab890e
IvoGarraza <ivogarrachip@gmail.com>
2024-04-15 15:37:48 -0300

    moviesapp

---
+++ src/app/movies/page.jsx
@@ -0,0 +1,12 @@
+export default function Page(){
+    return <div className="w-full flex flex-col">
+        <div className="h-12 w-full flex flex-col">Todas las peliculas
+            <div className="w-full">
+                <div>Filtros</div>
+            </div>
+        </div>
+        <div className="w-full">
+            
+        </div>
+    </div>
+}
